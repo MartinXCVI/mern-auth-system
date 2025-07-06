002d0b58da7a66727c0b64bbfb1d5489f8c70fcc
MartinXCVI <mdzg96@gmail.com>
2025-07-06 02:53:35 -0300

    Initial commit: Basic setup. // Dependencies installed: bcrypt, cookie-parser, cors, dotenv, express, jsonwebtoken, mongoose, nodemailer, zod. // devDependencies installed: @types/cookie-parser, @types/cors, @types/express, @types/node, nodemon, ts-node, typescript.

---
+++ backend/src/server.ts
@@ -0,0 +1,30 @@
+/* ENVIRONMENT VARIABLES */
+import { NODE_ENV, PORT_ENV } from './config/env.js'
+
+/* SERVER SETUP */
+import express from 'express'
+import { Request, Response } from 'express'
+import connectDB from './config/dbConnection.js'
+
+const app = express()
+const PORT = PORT_ENV || 3500
+connectDB()
+
+/* MIDDLEWARES */
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+
+// Parsing incoming requests with JSON
+app.use(express.json())
+// Parses incoming cookie headers into objects
+app.use(cookieParser())
+// Allowing CORS operations
+app.use(cors({ credentials: true }))
+
+console.log(`Mode: ${NODE_ENV}`)
+
+app.get('/', (req: Request, res: Response)=> { res.send("API Working") })
+
+app.listen(PORT, ()=> {
+  console.log(`Server running on port: ${PORT}...`)
+})
